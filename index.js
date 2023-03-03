febb29dab10a3ccbea26dc3b6305ddf1abda4527
Hoperaisers <abdallahsafari@gmail.com>
2023-03-03 14:15:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+
+    const input = document.getElementById('button');
+
+    function clickkAlert() {
+        alert('I was pressed!');
+    }
+
+    input.addEventListener('click', clickkAlert);
+
+
 }
